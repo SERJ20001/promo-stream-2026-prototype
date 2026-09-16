10e6d3b0a5fa55ae7ec9d605a50575b2477c9c62
Holy Mary <s.mihaylyuk@yandex.ru>
2026-09-16 17:20:27 +0300

    Стабилизировать иконку подсказки

--- advantages-viewer/preload.js
+++ advantages-viewer/preload.js
@@ -23,8 +23,8 @@
     { path: 'fonts/AvitoSansText-Bold.woff2', family: 'Avito Text', weight: '700' },
     { path: 'fonts/AvitoSansDisplay-Bold.woff2', family: 'Avito Display', weight: '700' }
   ];
-  const styles = ['style.css?v=89', 'alternative.css?v=92', 'viewer.css?v=89'];
-  const scripts = ['viewer.js?v=84', 'recommendation-model.js?v=84', 'recommendations.js?v=92', 'app.js?v=89', 'intro.js?v=84', 'confetti.js?v=84', 'heart-balloon.js?v=84'];
+  const styles = ['style.css?v=89', 'alternative.css?v=93', 'viewer.css?v=89'];
+  const scripts = ['viewer.js?v=84', 'recommendation-model.js?v=84', 'recommendations.js?v=93', 'app.js?v=89', 'intro.js?v=84', 'confetti.js?v=84', 'heart-balloon.js?v=84'];
   const resources = [...styles, ...images, ...fonts.map(font => font.path), ...scripts];
   const completed = new Map();
   const executed = new Set();
